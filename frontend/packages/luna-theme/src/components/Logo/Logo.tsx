0cb6c6598152df30a4c7a86667e60d917a47580e
Teodor Voicu <teodorvoicu87@gmail.com>
2025-10-25 01:52:34 +0300

    implement toggle theme button

--- frontend/packages/luna-theme/src/components/Logo/Logo.tsx
+++ frontend/packages/luna-theme/src/components/Logo/Logo.tsx
@@ -4,19 +4,25 @@
  */
 
 import React from 'react';
-import lunaLogoSVG from '../../icons/luna-logo.svg';
+import moonSVG from '../../icons/moon.svg';
+import sunSVG from '../../icons/theme-toggle.svg';
 import Icon from '@plone/volto/components/theme/Icon/Icon';
 
 interface LogoProps {
   pathname?: string;
   onToggleTheme?: () => void;
+  isDarkTheme?: boolean;
 }
 
-const Logo: React.FC<LogoProps> = ({ onToggleTheme }) => {
+const Logo: React.FC<LogoProps> = ({ onToggleTheme, isDarkTheme = false }) => {
   return (
-    <div className="luna-logo-wrapper" onClick={onToggleTheme} style={{ cursor: 'pointer' }} aria-label="Theme">
+    <div className="luna-logo-wrapper" onClick={onToggleTheme} style={{ cursor: 'pointer' }} aria-label="Toggle Theme">
       <div className="luna-logo">
-        <Icon name={lunaLogoSVG} size="32px" className="luna-logo-icon" />
+        <Icon
+          name={isDarkTheme ? sunSVG : moonSVG}
+          size="32px"
+          className="luna-logo-icon"
+        />
       </div>
     </div>
   );
